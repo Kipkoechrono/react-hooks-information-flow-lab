3fe2b893471e86598a1352fb9c30936c9b832b07
Kipkoechrono <ykentrell545@gmail.com>
2024-08-20 10:54:15 +0300

    creates filter for the items

--- .history/src/components/App_20240819055747.js
+++ .history/src/components/App_20240819055747.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
-import
+
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
